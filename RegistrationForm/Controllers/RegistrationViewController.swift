2898c16e3794aae309444fa73800d7f346ae45d1
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-10-22 20:59:33 +0300

    Code refactoring

--- RegistrationForm/Controllers/RegistrationViewController.swift
+++ RegistrationForm/Controllers/RegistrationViewController.swift
@@ -2,31 +2,43 @@ import UIKit
 
 final class RegistrationViewController: UIViewController {
     // MARK: - Constants
+    // UIImage
     private let backgroundImage = UIImage(named: "backgroundImage")
-    private let balinaSoftImage = UIImage(named: "logoImage")
+    private let logoImage = UIImage(named: "logoImage")
+
+    // UIImageView
+    private let backgroundImageView = UIImageView()
+    private let logoImageView = UIImageView()
 
     // MARK: - Properties
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        let backgroundImageView = UIImageView(image: backgroundImage)
-        let balinaSoftImageView = UIImageView(image: balinaSoftImage)
+        setupView()
+        setupBackgroundImageView()
+        setupLogoImageView()
+    }
+
+    // MARK: - Setups
+    private func setupView() {
         view.addSubview(backgroundImageView)
-        view.addSubview(balinaSoftImageView)
-        backgroundImageView.anchor(
-            top: view.topAnchor,
-            leading: view.safeAreaLayoutGuide.leadingAnchor,
-            trailing: view.safeAreaLayoutGuide.trailingAnchor,
-            bottom: view.bottomAnchor
-        )
-        balinaSoftImageView.anchor(
+        view.addSubview(logoImageView)
+    }
+
+    private func setupBackgroundImageView() {
+        backgroundImageView.image = backgroundImage
+        backgroundImageView.fillEntireView()
+    }
+
+    private func setupLogoImageView() {
+        logoImageView.image = logoImage
+        logoImageView.contentMode = .center
+        logoImageView.anchor(
             top: view.safeAreaLayoutGuide.topAnchor,
             leading: view.leadingAnchor,
             trailing: view.trailingAnchor,
             bottom: nil,
             padding: .init(top: 100, left: 16, bottom: 0, right: 16)
         )
-
-        balinaSoftImageView.contentMode = .center
     }
 }

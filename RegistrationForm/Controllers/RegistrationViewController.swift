4b5e5fd4c410ac0f752d35e48cb26f8350acc938
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-10-22 20:43:02 +0300

    Added new extension for UIView to setting constraints Realisation backgroundImage and balinaSoftImage in RegistrationViewController

--- RegistrationForm/Controllers/RegistrationViewController.swift
+++ RegistrationForm/Controllers/RegistrationViewController.swift
@@ -2,10 +2,31 @@ import UIKit
 
 final class RegistrationViewController: UIViewController {
     // MARK: - Constants
+    private let backgroundImage = UIImage(named: "backgroundImage")
+    private let balinaSoftImage = UIImage(named: "logoImage")
+
     // MARK: - Properties
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .purple
+        let backgroundImageView = UIImageView(image: backgroundImage)
+        let balinaSoftImageView = UIImageView(image: balinaSoftImage)
+        view.addSubview(backgroundImageView)
+        view.addSubview(balinaSoftImageView)
+        backgroundImageView.anchor(
+            top: view.topAnchor,
+            leading: view.safeAreaLayoutGuide.leadingAnchor,
+            trailing: view.safeAreaLayoutGuide.trailingAnchor,
+            bottom: view.bottomAnchor
+        )
+        balinaSoftImageView.anchor(
+            top: view.safeAreaLayoutGuide.topAnchor,
+            leading: view.leadingAnchor,
+            trailing: view.trailingAnchor,
+            bottom: nil,
+            padding: .init(top: 100, left: 16, bottom: 0, right: 16)
+        )
+
+        balinaSoftImageView.contentMode = .center
     }
 }

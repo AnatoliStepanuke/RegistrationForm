4bccfd82212c10f1389c585f8bded8700aa604df
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-10-29 16:07:13 +0300

    Initial code refactoring branch

--- RegistrationForm/Controllers/RegistrationViewController.swift
+++ RegistrationForm/Controllers/RegistrationViewController.swift
@@ -34,6 +34,8 @@ final class RegistrationViewController: UIViewController {
     private let emailSeparatorView = RegistrationSeparatorView(color: AppColors.whiteColor)
     private let passwordSeparatorView = RegistrationSeparatorView(color: AppColors.whiteColor)
 
+    private let containerStackView = UIStackView()
+
     // MARK: - Properties
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -41,24 +43,26 @@ final class RegistrationViewController: UIViewController {
         setupView()
         setupBackgroundImageView()
         setupLogoImageView()
-        setupEmailLabel()
-        setupEmailTextField()
-        setupPasswordLabel()
-        setupPasswordTextField()
-        setupRegistrationButton()
+        setupContainerStackView()
+//        setupEmailLabel()
+//        setupEmailTextField()
+//        setupPasswordLabel()
+//        setupPasswordTextField()
+//        setupRegistrationButton()
     }
 
     // MARK: - Setups
     private func setupView() {
         view.addSubview(backgroundImageView)
         view.addSubview(logoImageView)
-        view.addSubview(emailLabel)
-        view.addSubview(emailTextField)
-        view.addSubview(emailSeparatorView)
-        view.addSubview(passwordLabel)
-        view.addSubview(passwordTextField)
-        view.addSubview(passwordSeparatorView)
-        view.addSubview(registrationButton)
+        view.addSubview(containerStackView)
+//        view.addSubview(emailLabel)
+//        view.addSubview(emailTextField)
+//        view.addSubview(emailSeparatorView)
+//        view.addSubview(passwordLabel)
+//        view.addSubview(passwordTextField)
+//        view.addSubview(passwordSeparatorView)
+//        view.addSubview(registrationButton)
     }
 
     private func setupBackgroundImageView() {
@@ -78,67 +82,78 @@ final class RegistrationViewController: UIViewController {
         )
     }
 
-    private func setupEmailLabel() {
-        emailLabel.anchor(
-            top: logoImageView.bottomAnchor,
-            leading: view.leadingAnchor,
-            trailing: view.trailingAnchor,
-            bottom: nil,
-            padding: .init(top: 100, left: 25, bottom: 0, right: 25)
-        )
+    private func setupContainerStackView() {
+        containerStackView.axis = .horizontal
+        containerStackView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        containerStackView.backgroundColor = .green
+        containerStackView.anchor(top: logoImageView.bottomAnchor,
+                                  leading: view.leadingAnchor,
+                                  trailing: view.trailingAnchor,
+                                  bottom: nil,
+                                  padding: .init(top: 100, left: 25, bottom: 0, right: 25))
     }
 
-    private func setupEmailTextField() {
-        emailTextField.anchor(
-            top: emailLabel.bottomAnchor,
-            leading: view.leadingAnchor,
-            trailing: view.trailingAnchor,
-            bottom: nil,
-            padding: .init(top: 15, left: 25, bottom: 0, right: 25)
-        )
-        emailSeparatorView.anchor(
-            top: emailTextField.bottomAnchor,
-            leading: emailTextField.leadingAnchor,
-            trailing: emailTextField.trailingAnchor,
-            bottom: nil,
-            padding: .init(top: 20, left: 0, bottom: 0, right: 0)
-        )
-    }
-
-    private func setupPasswordLabel() {
-        passwordLabel.anchor(
-            top: emailSeparatorView.bottomAnchor,
-            leading: view.leadingAnchor,
-            trailing: view.trailingAnchor,
-            bottom: nil,
-            padding: .init(top: 25, left: 25, bottom: 0, right: 25)
-        )
-    }
-
-    private func setupPasswordTextField() {
-        passwordTextField.anchor(
-            top: passwordLabel.bottomAnchor,
-            leading: view.leadingAnchor,
-            trailing: view.trailingAnchor,
-            bottom: nil,
-            padding: .init(top: 15, left: 25, bottom: 0, right: 25)
-        )
-        passwordSeparatorView.anchor(
-            top: passwordTextField.bottomAnchor,
-            leading: passwordTextField.leadingAnchor,
-            trailing: passwordTextField.trailingAnchor,
-            bottom: nil,
-            padding: .init(top: 20, left: 0, bottom: 0, right: 0)
-        )
-    }
-
-    private func setupRegistrationButton() {
-        registrationButton.anchor(
-            top: passwordSeparatorView.bottomAnchor,
-            leading: view.leadingAnchor,
-            trailing: view.trailingAnchor,
-            bottom: nil,
-            padding: .init(top: 40, left: 25, bottom: 0, right: 25)
-        )
-    }
+//    private func setupEmailLabel() {
+//        emailLabel.anchor(
+//            top: logoImageView.bottomAnchor,
+//            leading: view.leadingAnchor,
+//            trailing: view.trailingAnchor,
+//            bottom: nil,
+//            padding: .init(top: 100, left: 25, bottom: 0, right: 25)
+//        )
+//    }
+//
+//    private func setupEmailTextField() {
+//        emailTextField.anchor(
+//            top: emailLabel.bottomAnchor,
+//            leading: view.leadingAnchor,
+//            trailing: view.trailingAnchor,
+//            bottom: nil,
+//            padding: .init(top: 15, left: 25, bottom: 0, right: 25)
+//        )
+//        emailSeparatorView.anchor(
+//            top: emailTextField.bottomAnchor,
+//            leading: emailTextField.leadingAnchor,
+//            trailing: emailTextField.trailingAnchor,
+//            bottom: nil,
+//            padding: .init(top: 20, left: 0, bottom: 0, right: 0)
+//        )
+//    }
+//
+//    private func setupPasswordLabel() {
+//        passwordLabel.anchor(
+//            top: emailSeparatorView.bottomAnchor,
+//            leading: view.leadingAnchor,
+//            trailing: view.trailingAnchor,
+//            bottom: nil,
+//            padding: .init(top: 25, left: 25, bottom: 0, right: 25)
+//        )
+//    }
+//
+//    private func setupPasswordTextField() {
+//        passwordTextField.anchor(
+//            top: passwordLabel.bottomAnchor,
+//            leading: view.leadingAnchor,
+//            trailing: view.trailingAnchor,
+//            bottom: nil,
+//            padding: .init(top: 15, left: 25, bottom: 0, right: 25)
+//        )
+//        passwordSeparatorView.anchor(
+//            top: passwordTextField.bottomAnchor,
+//            leading: passwordTextField.leadingAnchor,
+//            trailing: passwordTextField.trailingAnchor,
+//            bottom: nil,
+//            padding: .init(top: 20, left: 0, bottom: 0, right: 0)
+//        )
+//    }
+//
+//    private func setupRegistrationButton() {
+//        registrationButton.anchor(
+//            top: passwordSeparatorView.bottomAnchor,
+//            leading: view.leadingAnchor,
+//            trailing: view.trailingAnchor,
+//            bottom: nil,
+//            padding: .init(top: 40, left: 25, bottom: 0, right: 25)
+//        )
+//    }
 }

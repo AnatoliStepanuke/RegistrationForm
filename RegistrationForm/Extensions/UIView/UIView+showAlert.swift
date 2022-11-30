5988b33639f0688cbd3f5fe55d971144196648e9
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-11-30 21:32:35 +0300

    AlertManager code refactoring

--- RegistrationForm/Extensions/UIView/UIView+showAlert.swift
+++ RegistrationForm/Extensions/UIView/UIView+showAlert.swift
@@ -1,16 +1,10 @@
 import UIKit
 
 extension UIViewController: RegistrationAlerts {
-    func showEmptyFieldsAlert() { self.present(AlertManager.instance.showEmptyFieldsAlert(), animated: true)}
-    func showEmptyEmailFieldAlert() { self.present(AlertManager.instance.showEmptyEmailFieldAlert(), animated: true)}
-    func showEmptyPasswordFieldAlert() {
-        self.present(AlertManager.instance.showEmptyPasswordFieldAlert(), animated: true)
-    }
-    func showSuccessAlert() { self.present(AlertManager.instance.showSuccessAlert(), animated: true)}
-    func showMinLengthPasswordAlert() {
-        self.present(AlertManager.instance.showMinLengthPasswordAlert(), animated: true)
-    }
-    func showMaxLengthPasswordAlert() {
-        self.present(AlertManager.instance.showMaxLengthPasswordAlert(), animated: true)
-    }
+    func showEmptyFieldsAlert() { self.present(AlertManager().showEmptyFieldsAlert(), animated: true) }
+    func showEmptyEmailFieldAlert() { self.present(AlertManager().showEmptyEmailFieldAlert(), animated: true) }
+    func showEmptyPasswordFieldAlert() { self.present(AlertManager().showEmptyPasswordFieldAlert(), animated: true) }
+    func showSuccessAlert() { self.present(AlertManager().showSuccessAlert(), animated: true) }
+    func showMinLengthPasswordAlert() { self.present(AlertManager().showMinLengthPasswordAlert(), animated: true) }
+    func showMaxLengthPasswordAlert() { self.present(AlertManager().showMaxLengthPasswordAlert(), animated: true) }
 }
